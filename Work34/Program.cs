269ac156c93194d70515505c0b04d7c8b35e0d57
Sergey <blackwhiterainbow@yandex.ru>
2023-01-31 20:51:26 +0300

    add Массивы. Вывод четных числел массива

---
+++ Work34/Program.cs
@@ -0,0 +1,46 @@
+﻿// // Задайте массив заполненный случайными положительными трёхзначными числами. 
+// //Напишите программу, которая покажет количество чётных чисел в массиве
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int elementsCount = int.Parse(Console.ReadLine()!);
+    
+int[] myArray = new int[elementsCount];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = new Random().Next(100, 1000);
+    
+}
+
+Console.WriteLine("Вывод элементов массива: ");
+Console.WriteLine(String.Join(", ", myArray));
+
+int[] mas = myArray;
+int num = 0;
+int count = 0;
+for (int j = 0; j < myArray.Length; j++)
+{
+    while (mas[j] > 0)
+    {
+        num = mas[j] % 10;
+        mas[j] = mas[j] / 10;
+
+        if (num % 2 == 0)
+        {
+            count++;
+        }
+        else
+        {
+            count = count + 0;
+        }
+    }    
+}
+
+Console.WriteLine("Вывод количества четных чисел: ");
+Console.WriteLine(String.Join(", ", count));
+
+
+
+
+
