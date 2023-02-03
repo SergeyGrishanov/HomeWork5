cb36cef2942dcc460c527e11b54a03217fc0532e
Sergey <blackwhiterainbow@yandex.ru>
2023-02-03 15:18:59 +0300

    add Сортировка пузырьком

---
+++ WorkStar/Program.cs
@@ -0,0 +1,36 @@
+﻿// алгоритм сортировки методом пузырька. 
+//Реализовать невозрастающую сортировку.
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int elementsCount = int.Parse(Console.ReadLine()!);
+    
+int[] myArray = new int[elementsCount];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = new Random().Next(1, 100);
+    
+}
+Console.WriteLine("Вывод элементов массива: ");
+Console.WriteLine(String.Join(", ", myArray));
+
+int[] mas = myArray;
+
+int temp = 0;
+
+for (int j = 0; j < mas.Length; j++) 
+{
+    for (int sort = 0; sort < mas.Length - 1; sort++) 
+    {
+        if (mas[sort] > mas[sort + 1]) {
+            temp = mas[sort + 1];
+            mas[sort + 1] = mas[sort];
+            mas[sort] = temp;
+        }
+    }
+}
+Console.WriteLine("Вывод элементов сортированного массива: ");
+for (int i = 0; i < mas.Length; i++)
+    
+    Console.Write(mas[i] + " ");
